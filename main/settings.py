c86e689751c62c5ba8db483278b940796c417389
EdoardoAvvi <167179629+EdoardoAvvi@users.noreply.github.com>
2024-06-20 18:14:13 +0200

    changes for deploy

--- main/settings.py
+++ main/settings.py
@@ -10,23 +10,31 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
+from environ import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-if os.path.exists('env.py'):
-    import env
+env = Env()
+Env().read_env()
+ENVIRONMENT = env("ENVIRONMENT", default="production")
+
+#if os.path.exists('env.py'):
+#    import env
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG') != 'False'
-#DEBUG = True
+if ENVIRONMENT == "development":
+    DEBUG = "True"
+else:
+    DEBUG = "False"
 
 ALLOWED_HOSTS = ["djangorailway-production.up.railway.app", "127.0.0.1", "localhost"]
 
@@ -140,6 +148,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
